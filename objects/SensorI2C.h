c8ce583ca6800a3ccbe7a51eba883c81c4652b2a
walkingtoucan <112744493+walkingtoucan@users.noreply.github.com>
2025-07-20 12:58:40 -0700

    initial implementation of sensorI2C

---
+++ objects/SensorI2C.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <WireManager.h>
+class WireManager;
+
+class SensorI2C {
+    private:
+        WireManager* wireManager;
+        int address;
+        int muxLine;
+
+    public:
+        SensorI2C(int address, int muxLine);
+        void assignWireManager(WireManager* wireManager);
+        int beginTransmission();
+        int endTransmission(bool stop=true);
+        int requestFrom(int address, int quantity, bool stop=true);
+        int available();
+        int write(int value);
+};
