93404c70b497211682194f9c764e3be1aa1e6ecd
brynnbelecki <brynnhmb@icloud.com>
2025-07-21 11:09:44 -0700

    Servo edit BB

--- test/ClawManager/src/main.cpp
+++ test/ClawManager/src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
+#include "../../../objects/Servo/Servo.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Servo* servo = new Servo(4, 1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  servo->begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+servo->writeDutyCycle(9.5);
+delay(500);
+servo->writeDutyCycle(10);
+delay(500);
+servo->writeDutyCycle(10.5);
+delay(500);
+servo->writeDutyCycle(11);
+delay(500);
+servo->writeDutyCycle(11.5);
+delay(500);
+servo->writeDutyCycle(12);
+delay(500);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+/** 
+  servo->writePosition(0);
+  delay(500);
+  servo->writePosition(10);
+  delay(500);
+  servo->writePosition(20);
+  delay(500);
+  servo->writePosition(30);
+  delay(500);
+  servo->writePosition(40);
+  delay(500);
+  servo->writePosition(50);
+  delay(500);
+  servo->writePosition(60);
+  delay(500);
+  servo->writePosition(70);
+  delay(500);
+  servo->writePosition(80);
+  delay(500);
+  servo->writePosition(90);
+  delay(500);
+  servo->writePosition(100);
+  delay(500);
+  servo->writePosition(110);
+  delay(500);
+  servo->writePosition(120);
+  delay(500); 
+  servo->writePosition(130);
+  delay(500);
+  servo->writePosition(140);
+  delay(500);
+  servo->writePosition(150);
+  delay(500);
+  servo->writePosition(160);
+  delay(500);
+  servo->writePosition(170);
+  delay(500);
+  servo->writePosition(180);
+  delay(500);
+*/ 
+
 }
