7da2bcbfcab0d4495d3f0f82824464f0cfab8e1f
walkingtoucan <112744493+walkingtoucan@users.noreply.github.com>
2025-07-27 00:35:09 -0700

    style

--- objects/claw/ClawVerticalStage.cpp
+++ objects/claw/ClawVerticalStage.cpp
@@ -7,22 +7,22 @@ ClawVerticalStage::ClawVerticalStage(int motorPin1, int motorPin2, int pwmChanne
     int switchPin, bool normallyOpen) : 
     ClawPart(motorPin1, motorPin2, pwmChannel1, pwmChannel2, muxLine, encoderOnTerminalSide, 
         switchPin, normallyOpen) {
-        this->partName = this->_partName; // For debugging purposes
+    this->partName = this->_partName; // For debugging purposes
 
-        // Conversion and positions
-        this->ENCODER_TO_POS_CONVERSION = this->_ENCODER_TO_POS_CONVERSION;
-        this->POS_TO_ENCODER_CONVERSION = this->_POS_TO_ENCODER_CONVERSION;
-        this->ABS_POS_LIMIT = this->_ABS_POS_LIMIT; // Position limit (for homing purposes)
-        this->MIN_POSITION = this->_MIN_POSITION;
-        this->MAX_POSITION = this->_MAX_POSITION;
+    // Conversion and positions
+    this->ENCODER_TO_POS_CONVERSION = this->_ENCODER_TO_POS_CONVERSION;
+    this->POS_TO_ENCODER_CONVERSION = this->_POS_TO_ENCODER_CONVERSION;
+    this->ABS_POS_LIMIT = this->_ABS_POS_LIMIT; // Position limit (for homing purposes)
+    this->MIN_POSITION = this->_MIN_POSITION;
+    this->MAX_POSITION = this->_MAX_POSITION;
 
-        // PID Parameters
-        this->Pk = this->_Pk;
-        this->Dk = this->_Dk;
+    // PID Parameters
+    this->Pk = this->_Pk;
+    this->Dk = this->_Dk;
 
-        // Servo Parameters
-        this->servoMaxVoltage = this->_servoMaxVoltage;
-        this->servoTolerance = this->_servoTolerance; // How much can angle deviate from target
+    // Servo Parameters
+    this->servoMaxVoltage = this->_servoMaxVoltage;
+    this->servoTolerance = this->_servoTolerance; // How much can angle deviate from target
 }
 
 void ClawVerticalStage::testSequence() {
