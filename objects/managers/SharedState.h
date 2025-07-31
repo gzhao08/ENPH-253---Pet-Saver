b9039ffecfe65daeab73e5be92449b3e48964170
kayjew <kjewel02@student.ubc.ca>
2025-07-31 10:37:36 -0700

    finished window sensing test code

--- objects/managers/SharedState.h
+++ objects/managers/SharedState.h
@@ -5,8 +5,8 @@ extern volatile boolean startRead; // Flag to indicate if reading should start
 extern volatile boolean drive; // Flag to indicate if driving should start
 extern volatile int currentSpeed;
 
-void startRead();
-void stopRead();    
+void startReading();
+void stopReading();    
 void stopDrive();
 void startDrive();
 void changeSpeed(int speed);
